e89b22faea078fc620ddc533a4de083b7f4a3daf
Léo Depriester <leo.depriester@camptocamp.com>
2019-04-16 17:22:02 +0200

    cleanup options

--- lib/train-openstack/connection.rb
+++ lib/train-openstack/connection.rb
@@ -7,38 +7,8 @@ module TrainPlugins
 			include TrainPlugins::OpenstackClient::Platform
 
 			def initialize(options)
-				os_auth_url = ENV['OS_AUTH_URL'],
-				os_domain_name = ENV['OS_DOMAIN_NAME'],
-				os_identity_api_version = ENV['OS_IDENTITY_API_VERSION'],
-				os_image_api_version = ENV['OS_IMAGE_API_VERSION'],
-				os_password = ENV['OS_PASSWORD'],
-				os_project_domain_name = ENV['OS_PROJECT_DOMAIN_NAME'],
-				os_project_name = ENV['OS_PROJECT_NAME'],
-				os_region_name = ENV['OS_REGION_NAME'],
-				os_tenant_name = ENV['OS_TENANT_NAME'],
-				os_username = ENV['OS_USERNAME'],
-				os_user_domain_name = ENV['OS_USER_DOMAIN_NAME'],
-				os_version = ENV['OS_VERSION'],
-				os_volume_api_version = ENV['OS_VOLUME_API_VERSION']
-
-				@opts = {
-					api_key: os_password,
-					auth_method: 'password',
-					authtenant_name: os_tenant_name,
-					auth_url: os_auth_url,
-					username: os_username,
-					user_domain: os_user_domain_name,
-					project_name: os_project_name,
-					project_domain_name: os_project_domain_name,
-					region: os_region_name
-				}
+				@opts = options
 				super(options)
-
-				begin
-					@client = Openstack::Connection.create(@opts)
-				rescue => e
-					raise Train::TransportError, e
-				end
 			end
 
 			def local?
@@ -49,8 +19,9 @@ module TrainPlugins
 				'openstack://'
 			end
 
-			def client
-				@client
+			def openstack_resource
+				@opts[:service_type] = service_type
+				OpenStack::Connection.create(@opts)
 			end
 		end
 	end
--- lib/train-openstack/transport.rb
+++ lib/train-openstack/transport.rb
@@ -5,19 +5,16 @@ module TrainPlugins
 		class Transport < Train.plugin(1)
 			name 'openstack'
 
-			option :server, required: true, default: ENV['PUPPETDB_SERVER']
-			option :key, default: ENV['PUPPETDB_KEY']
-			option :cert, default: ENV['PUPPETDB_CERT']
-			option :cacert, default: ENV['PUPPETDB_CACERT']
-			option :auth_url, default: ENV['OS_AUTH_URL'],
-			option :domain_name, default: ENV['OS_DOMAIN_NAME']
-			option :password, default: ENV['OS_PASSWORD']
+			option :auth_url, default: ENV['OS_AUTH_URL']
+			option :auth_method, default: 'password'
+			option :username, default: ENV['OS_USERNAME']
+			option :api_key, default: ENV['OS_PASSWORD']
+			option :user_domain, default: ENV['OS_USER_DOMAIN_NAME']
 			option :project_domain_name, default: ENV['OS_PROJECT_DOMAIN_NAME']
 			option :project_name, default: ENV['OS_PROJECT_NAME']
-			option :region_name, default: ENV['OS_REGION_NAME']
-			option :tenant_name, default: ENV['OS_TENANT_NAME']
-			option :username, default: ENV['OS_USERNAME']
-			option :user_domain_name, default: ENV['OS_USER_DOMAIN_NAME']
+			option :region, default: ENV['OS_REGION_NAME']
+			option :authtenant_name, default: ENV['OS_TENANT_NAME']
+			option :authtenant_id, default: ENV['OS_TENANT_ID']
 
 			def connection(_instance_opts = nil)
 				@connection ||= TrainPlugins::OpenstackClient::Connection.new(@options)
